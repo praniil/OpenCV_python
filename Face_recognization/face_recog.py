28b11ce761ec5014c551ffaed33aa515841e5ec1
praniil <pparajuli15@gmail.com>
2024-01-27 21:47:12 +0545

    feat: added beep sound as soon as face is recognized

--- Face_recognization/face_recog.py
+++ Face_recognization/face_recog.py
@@ -1,4 +1,5 @@
 import cv2
+import pygame
 
 def faceRec():
     face_cascade = cv2.CascadeClassifier('/home/pranil/.local/lib/python3.10/site-packages/cv2/data/haarcascade_frontalface_default.xml')
@@ -8,12 +9,16 @@ def faceRec():
         ret, img = cap.read()       #reads frames from a camera
         if not ret:
             break
-
+        #cvtColor converts the rgb to gray
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)    #img and gray are numPy array i.e a multi dimensional array: matrix
+
+        #detectMultiScale returns a list of rectanges representing the detected faces
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        pygame.mixer.init()
+        sound = pygame.mixer.Sound('/home/pranil/pythonProjects/openCV/Face_recognization/beep-01a.wav')
 
         for (x, y, w, h) in faces:
-            cv2.rectangle(img, (x, y), (x+w, y+h), (255,255,0), 2)
+            cv2.rectangle(img, (x, y), (x+w, y+h), (255,255,0), 2)      #x, y coordinates of top left rectange on the face w and h are width , 225, 225, 0 color of rectange, with thickness 2
             roi_gray = gray[y:y+h, x:x+w]
             roi_color = img[y:y+h, x:x+w]
 
@@ -22,7 +27,8 @@ def faceRec():
             for (ex, ey, ew, eh) in eyes:
                 cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 127, 255), 2)
 
-    
+            sound.play()
+
         cv2.imshow('img', img)
         k = cv2.waitKey(30) & 0xff
         if k == 27:
